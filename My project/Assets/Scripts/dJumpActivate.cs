a327e2637b172139fcf166a14ae497f218d8ef2a
Malte Paulmann <malte@rpaulmann.de>
2022-06-02 01:02:44 +0200

    add animations/ script collect/respawn fruit

--- My project/Assets/Scripts/dJumpActivate.cs
+++ My project/Assets/Scripts/dJumpActivate.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class dJumpActivate : MonoBehaviour
 {
+    private enum animationStates { idle, activated}
+
     [SerializeField] private bool isForDJump;
     [SerializeField] private bool isForDash;
     public GameObject Player;
+    private Animator anim;
+
+    private bool activate;
+    [SerializeField] private float cdTime;
+    private float timer;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !activate)
         {
             if (isForDJump)
             {
@@ -22,6 +34,43 @@ public class dJumpActivate : MonoBehaviour
             {
                 Player.GetComponent<PlayerMovement>().dashReady = true;
             }
+            activate = true;
         }
+    }
+    private void Update()
+    {
+        if (activate)
+        {
+            timer -= Time.deltaTime;
+            if ( timer <= 0)
+            {
+                activate = false;
+                timer = cdTime;
+            }
+        }
+        animationStates state;
+        if (activate)
+        {
+            state = animationStates.activated;
+        }
+        else
+        {
+            state = animationStates.idle;
+        }
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void UpdateAnimationState()
+    {
+        animationStates state;
+        if (activate)
+        {
+            state = animationStates.activated;
+        }
+        else
+        {
+            state = animationStates.idle;
+        }
+        anim.SetInteger("state", (int)state);
     }
 }
