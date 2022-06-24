a47de7f2a0bc3a02ce92283c1e3a2b7f050b1b21
Bt-tr <jan@holznagel.net>
2022-06-24 23:47:43 +0200

    Timer(Script)

---
+++ My project/Assets/Timer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 0;
+    public Text textBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart += Time.deltaTime;
+        textBox.text = timeStart.ToString("F2");
+    }
+}
